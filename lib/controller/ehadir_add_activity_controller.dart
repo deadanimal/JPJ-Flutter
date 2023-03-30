05417c50ac0f572b34eaa7d32521bbda2495834e
Amir <amirrasyid@outlook.com>
2023-03-30 23:50:01 +0800

    jpj eHadir new API structure

--- lib/controller/ehadir_add_activity_controller.dart
+++ lib/controller/ehadir_add_activity_controller.dart
@@ -8,8 +8,10 @@ import 'package:jpj_info/controller/appbar_controller.dart';
 import 'package:jpj_info/controller/bottom_nav_controller.dart';
 import 'package:jpj_info/controller/http_request_controller.dart';
 import 'package:jpj_info/helper/account_manager.dart';
+import 'package:jpj_info/helper/ehadir_session_check.dart';
 import 'package:jpj_info/helper/string_helper.dart';
 import 'package:jpj_info/model/ehadir/activity_list_res.dart';
+import 'package:jpj_info/model/ehadir/edit_activiti_req.dart';
 import 'package:jpj_info/model/ehadir/new_activity_req.dart';
 import 'package:jpj_info/model/ehadir/new_activity_res.dart';
 import 'package:jpj_info/view/appBarHeader/gradient_decor.dart';
@@ -173,6 +175,15 @@ class _EhadirAddActivityController extends State<EhadirAddActivityController> {
     }
   }
 
+  _checkendDate() {
+    DateTime startDate = DateTime.parse(selectedDate);
+    endDate = DateFormat('yyyy-MM-dd').format(
+      startDate.add(
+        Duration(days: int.parse(noOfDays.text) - 1),
+      ),
+    );
+  }
+
   void _pickDate() async {
     DateTime? pickedDate = await showDatePicker(
         context: context,
@@ -321,7 +332,7 @@ class _EhadirAddActivityController extends State<EhadirAddActivityController> {
     );
   }
 
-  _update() {
+  _update() async {
     if (activityName.text.isNotEmpty &&
         noOfDays.text.isNotEmpty &&
         date.text.isNotEmpty &&
@@ -329,37 +340,168 @@ class _EhadirAddActivityController extends State<EhadirAddActivityController> {
         startTime.text.isNotEmpty &&
         endTime.text.isNotEmpty &&
         agenda.text.isNotEmpty) {
-      SiteConfig conf = SiteConfig();
-      NewActivityReq req = NewActivityReq(
-        nokp: MyJPJAccountManager().id,
-        bilanganHari: noOfDays.text,
-        lokasi: location.text,
-        keterangan: agenda.text,
-        nama: activityName.text,
-        bilanganSesi: sessionPerDay.text,
-        tarikhMula: selectedDate,
-        tarikhTamat: endDate,
-        latitude: latitude.text,
-        longitude: longitude.text,
-        masaMula1: startTime.text,
-        masaTamat1: endTime.text,
-        masaMula2: startTime1.text,
-        masaTamat2: endTime1.text,
-        masaMula3: startTime2.text,
-        masaTamat3: endTime2.text,
-        masaMula4: startTime3.text,
-        masaTamat4: endTime3.text,
-        idAktiviti: widget.activityId,
-      );
+      if (_checkSessions()) {
+        _checkendDate();
+        SiteConfig conf = SiteConfig();
+        EditActivityReq req = EditActivityReq(
+          nokp: MyJPJAccountManager().id,
+          bilanganHari: noOfDays.text,
+          lokasi: location.text,
+          keterangan: agenda.text,
+          nama: activityName.text,
+          bilanganSesi: sessionPerDay.text,
+          tarikhMula: selectedDate,
+          tarikhTamat: endDate,
+          latitude: latitude.text,
+          longitude: longitude.text,
+          idAktiviti: widget.activityId,
+          sesi: _getSessionData(),
+        );
 
-      return jpjHttpRequest(
-        context,
-        Uri.parse(conf.eHadirUpdateActivity),
-        headers: conf.formHeader,
-        body: jsonEncode(req.toJson()),
-        callback: _submitActivityCallback,
+        Map<String, dynamic> map = {};
+        map["nokp"] = MyJPJAccountManager().id;
+        map["bilangan_hari"] = noOfDays.text;
+        map["lokasi"] = location.text;
+        map["keterangan"] = agenda.text;
+        map["nama"] = activityName.text;
+        map["bilangan_sesi"] = sessionPerDay.text;
+        map["tarikh_mula"] = selectedDate;
+        map["tarikh_tamat"] = endDate;
+        map["latitude"] = latitude.text;
+        map["longitude"] = longitude.text;
+        map["id_aktiviti"] = widget.activityId.toString();
+        map["sesi[0][masa_mula]"] = startTime.text;
+        map["sesi[0][masa_tamat]"] = endTime.text;
+
+        if (startTime1.text != '' && endTime1.text != '') {
+          map["sesi[1][masa_mula]"] = startTime1.text;
+          map["sesi[1][masa_tamat]"] = endTime1.text;
+        }
+
+        if (startTime2.text != '' && endTime2.text != '') {
+          map["sesi[2][masa_mula]"] = startTime2.text;
+          map["sesi[2][masa_tamat]"] = endTime2.text;
+        }
+
+        if (startTime3.text != '' && endTime3.text != '') {
+          map["sesi[3][masa_mula]"] = startTime3.text;
+          map["sesi[3][masa_tamat]"] = endTime3.text;
+        }
+
+        return jpjHttpRequest(
+          context,
+          Uri.parse(conf.eHadirUpdateActivity),
+          body: map,
+          callback: _submitActivityCallback,
+        );
+      } else {
+        AlertController(ctx: context).generalError(
+          AppLocalizations.of(context)!.invalidTime,
+          () {
+            Navigator.pop(context);
+          },
+        );
+      }
+    } else {
+      AlertController(ctx: context).generalSpanError(
+        AppLocalizations.of(context)!.pleaseFillInRequired,
+        ' *',
+        () {
+          Navigator.pop(context);
+        },
+        textStyle2: const TextStyle(
+          color: Colors.red,
+        ),
       );
-    } else {}
+    }
+  }
+
+  List<Sesi> _getSessionData() {
+    List<Sesi> sessions = [];
+    sessions.add(Sesi(
+      masaMula: startTime.text,
+      masaTamat: endTime.text,
+    ));
+
+    if (startTime1.text != '' && endTime1.text != '') {
+      sessions.add(Sesi(
+        masaMula: startTime1.text,
+        masaTamat: endTime1.text,
+      ));
+    }
+
+    if (startTime2.text != '' && endTime2.text != '') {
+      sessions.add(Sesi(
+        masaMula: startTime2.text,
+        masaTamat: endTime2.text,
+      ));
+    }
+
+    if (startTime3.text != '' && endTime3.text != '') {
+      sessions.add(Sesi(
+        masaMula: startTime3.text,
+        masaTamat: endTime3.text,
+      ));
+    }
+
+    return sessions;
+  }
+
+  _checkSessions() {
+    var retval1 = false;
+    var retval2 = true;
+    var retval3 = true;
+    var retval4 = true;
+    if (startTime.text != '' && endTime.text != '') {
+      if (_validateTime(startTime.text, endTime.text)) {
+        retval1 = true;
+      } else {
+        retval1 = false;
+      }
+    }
+
+    if (startTime1.text != '' && endTime1.text != '') {
+      if (_validateTime(startTime1.text, endTime1.text)) {
+        retval2 = true;
+      } else {
+        retval2 = false;
+      }
+    }
+
+    if (startTime2.text != '' && endTime2.text != '') {
+      if (_validateTime(startTime2.text, endTime2.text)) {
+        retval3 = true;
+      } else {
+        retval3 = false;
+      }
+    }
+
+    if (startTime3.text != '' && endTime3.text != '') {
+      if (_validateTime(startTime3.text, endTime3.text)) {
+        retval4 = true;
+      } else {
+        retval4 = false;
+      }
+    }
+
+    return retval1 && retval2 && retval3 && retval4;
+  }
+
+  _validateTime(String start, String end) {
+    var rawStart = start.split(':');
+    var rawEnd = end.split(':');
+    var sessionStartTime = TimeOfDay(
+      hour: int.parse(rawStart[0]),
+      minute: int.parse(rawStart[1]),
+    );
+    var sessionEndTime = TimeOfDay(
+      hour: int.parse(rawEnd![0]),
+      minute: int.parse(rawEnd[1]),
+    );
+    var startDouble = toDouble(sessionStartTime);
+    var endDouble = toDouble(sessionEndTime);
+
+    return endDouble > startDouble;
   }
 
   _create() {
@@ -370,35 +512,56 @@ class _EhadirAddActivityController extends State<EhadirAddActivityController> {
         startTime.text.isNotEmpty &&
         endTime.text.isNotEmpty &&
         agenda.text.isNotEmpty) {
-      SiteConfig conf = SiteConfig();
-      NewActivityReq req = NewActivityReq(
-        nokp: MyJPJAccountManager().id,
-        bilanganHari: noOfDays.text,
-        lokasi: location.text,
-        keterangan: agenda.text,
-        nama: activityName.text,
-        bilanganSesi: sessionPerDay.text,
-        tarikhMula: selectedDate,
-        latitude: latitude.text,
-        longitude: longitude.text,
-        masaMula1: startTime.text,
-        masaTamat1: endTime.text,
-        masaMula2: startTime1.text,
-        masaTamat2: endTime1.text,
-        masaMula3: startTime2.text,
-        masaTamat3: endTime2.text,
-        masaMula4: startTime3.text,
-        masaTamat4: endTime3.text,
-      );
+      if (_checkSessions()) {
+        _checkendDate();
+        SiteConfig conf = SiteConfig();
+        NewActivityReq req = NewActivityReq(
+          nokp: MyJPJAccountManager().id,
+          bilanganHari: noOfDays.text,
+          lokasi: location.text,
+          keterangan: agenda.text,
+          nama: activityName.text,
+          bilanganSesi: sessionPerDay.text,
+          tarikhMula: selectedDate,
+          latitude: latitude.text,
+          longitude: longitude.text,
+          masaMula1: startTime.text,
+          masaTamat1: endTime.text,
+          masaMula2: startTime1.text,
+          masaTamat2: endTime1.text,
+          masaMula3: startTime2.text,
+          masaTamat3: endTime2.text,
+          masaMula4: startTime3.text,
+          masaTamat4: endTime3.text,
+        );
 
-      return jpjHttpRequest(
-        context,
-        Uri.parse(conf.eHadirNewActivity),
-        headers: conf.formHeader,
-        body: jsonEncode(req.toJson()),
-        callback: _submitActivityCallback,
+        return jpjHttpRequest(
+          context,
+          Uri.parse(conf.eHadirNewActivity),
+          headers: conf.formHeader,
+          body: jsonEncode(req.toJson()),
+          callback: _submitActivityCallback,
+        );
+      } else {
+        AlertController(ctx: context).generalError(
+          AppLocalizations.of(context)!.invalidTime,
+          () {
+            Navigator.pop(context);
+          },
+        );
+      }
+    } else {
+      AlertController(ctx: context).generalSpanError(
+        AppLocalizations.of(context)!.pleaseFillInRequired,
+        '*',
+        () {
+          Navigator.pop(context);
+        },
+        textStyle2: const TextStyle(
+          color: Colors.red,
+        ),
       );
-    } else {}
+    }
   }
 
   _addSession() {
